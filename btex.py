ba6b1f70d7bd04e500f7bd771f6312bb38913d5d
toni-heittola <toni.heittola@gmail.com>
2024-11-01 10:20:27 +0200

    Update update_based_on_source function

--- btex.py
+++ btex.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Publication list plugin for Pelican
@@ -1629,8 +1630,8 @@ def btex(content):
                     use_scholarly0 = False
                     use_scholarly1 = False
                     try:
-                        from scholarly import scholarly
-                        from scholarly import ProxyGenerator, DOSException, MaxTriesExceededException
+                        from scholary import scholarly
+                        from scholary import ProxyGenerator, DOSException, MaxTriesExceededException
 
                         if btex_settings['google_scholar']['proxy']:
                             pg = ProxyGenerator()
@@ -1839,11 +1840,11 @@ def btex(content):
                                         )
 
                                     else:
-                                        update_citation_data_empty(
-                                            citation_data=citation_data,
-                                            title=pub['title'],
-                                            year=pub['year']
-                                        )
+                                        #update_citation_data_empty(
+                                        #    citation_data=citation_data,
+                                        #    title=pub['title'],
+                                        #    year=pub['year']
+                                        #)
 
                                         logger.warning(
                                             '[btex]    Nothing returned, article might not be indexed by Google or your access quota is exceeded!')
